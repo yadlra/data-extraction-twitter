c18a8cf37243677f7bd862f00de23a6214130801
yadlra <yadi.sb1@gmail.com>
2020-04-10 14:31:00 +0100

    whoops, set twitter credentials vars

--- mining_tweets.py
+++ mining_tweets.py
@@ -11,6 +11,11 @@ from unidecode import unidecode
 import datetime
 import pandas
 
+access_token = " "
+access_token_secret = " "
+consumer_key = " "
+consumer_secret = " "
+
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
