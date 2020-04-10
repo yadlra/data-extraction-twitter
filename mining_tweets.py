d54dc0fe86eed3d98f72406326ae30ae7f7ceb0c
yadlra <yadi.sb1@gmail.com>
2020-04-10 14:10:54 +0100

    setting user var and csv file name to be ceated with specific data

--- mining_tweets.py
+++ mining_tweets.py
@@ -15,3 +15,45 @@ import pandas
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
+
+users = []
+
+
+with open('tweets.csv', 'wb') as file:
+    writer = unicodecsv.writer(file, delimiter=',', quotechar='"')
+
+    writer.writerow(["user_name",
+                     "user_username",
+                     "user_followers_count",
+                     "user_listed_count",
+                     "user_following",
+                     "user_favorites",
+                     "user_verified",
+                     "user_default_profile",
+                     "user_location",
+                     "user_time_zone",
+                     "user_statuses_count",
+                     "user_description",
+                     "user_geo_enabled",
+                     "user_contributors_enabled",
+                     "tweet_year",
+                     "tweet_month",
+                     "tweet_day",
+                     "tweet_hour",
+                     "tweet_text",
+                     "tweet_lat",
+                     "tweet_long",
+                     "tweet_source",
+                     "tweet_in_reply_to_screen_name",
+                     "tweet_direct_reply",
+                     "tweet_retweet_status",
+                     "tweet_retweet_count",
+                     "tweet_favorite_count",
+                     "tweet_hashtags",
+                     "tweet_hashtags_count",
+                     "tweet_urls",
+                     "tweet_urls_count",
+                     "tweet_user_mentions",
+                     "tweet_user_mentions_count",
+                     "tweet_media_type",
+                     "tweet_contributors"])
